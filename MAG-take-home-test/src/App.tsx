e33407f02f027a2771db43bae97f77ef29c9e262
Mattsson717 <83518778+Mattsson717@users.noreply.github.com>
2022-04-12 11:30:51 +0200

    trying to fix the property value does not exist on Type eventTarget Error

--- MAG-take-home-test/src/App.tsx
+++ MAG-take-home-test/src/App.tsx
@@ -1,17 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, ChangeEvent } from 'react';
 import './App.css';
-import { calculatePrice } from '../refactor/price-calculator';
 
 const App = () => {
-  // const [user, setUser] = useState('');
-  // const [product, setProduct] = useState('');
-  // const [publishedToday, setPublishedToday] = useState('');
-  // const [totalPrice, setTotalPrice] = useState(0);
+  const [user, setUser] = useState<number | undefined>(0);
+  const [product, setProduct] = useState<number | undefined>(0);
+  const [publishedToday, setPublishedToday] = useState<string | undefined>(
+    'old'
+  );
+  // const [price, setPrice] = useState<number | undefined>(0);
+  const [totalPrice, setTotalPrice] = useState<number | undefined>(0);
 
-  let [user, setUser] = useState<string | undefined>('');
-  let [product, setProduct] = useState<string | undefined>('');
-  let [publishedToday, setPublishedToday] = useState<string | undefined>('');
-  let [totalPrice, setTotalPrice] = useState<number | undefined>(0);
+  const productPrice: number = 200;
+  const newProductPrice: number = 25;
+  const oldProductPrice: number = 35;
+  const companyRebate: number = 5;
+  const publishedTodayRebate: number = 10;
+
+  const onChangeHandler = (e: ChangeEvent) => {
+    const newValue = (e.target as HTMLInputElement).value;
+  };
+
+  const calculatePrice = () => {
+    if (user === 1 && product === 0) {
+      setTotalPrice(productPrice + newProductPrice - companyRebate);
+    } else if (user === 1 && product === 1) {
+      setTotalPrice(productPrice + oldProductPrice - companyRebate);
+    } else if (user === 0 && product === 0) {
+      setTotalPrice(productPrice + newProductPrice);
+    } else {
+      setTotalPrice(productPrice + oldProductPrice);
+    }
+  };
 
   return (
     <div className='App'>
@@ -19,72 +38,68 @@ const App = () => {
       <form onSubmit={(e) => e.preventDefault()}>
         <label>
           Select user type:
-          <select
-            defaultValue={'default'}
-            onChange={(e) => setUser(e.target.value)}
-            required
-          >
+          <select defaultValue='default' onChange={onChangeHandler} required>
             <option id='user' value='default' disabled>
               Choose
             </option>
 
-            <option id={user} value={0}>
-              Private user
-            </option>
+            <option value={0}>Private user</option>
 
-            <option id={user} value={1}>
-              Company user
-            </option>
+            <option value={1}>Company user</option>
           </select>
         </label>
 
         <label>
           Product type:
-          <select
-            defaultValue={'default'}
-            onChange={(e) => setProduct(e.target.value)}
-            required
-          >
+          <select defaultValue='default' onChange={onChangeHandler} required>
             <option id='productType' value='default' disabled>
               Choose
             </option>
-            <option id={product} value={0}>
+            <option id={'product'} value={0}>
               New
             </option>
-            <option id={product} value={1}>
+            <option id={'product'} value={1}>
               Old
             </option>
           </select>
         </label>
 
-        <label>
+        {/* <label>
           Price:
           <input
             value={totalPrice}
             type='number'
-            onChange={(e) => setProduct(e.target.value)}
+            onChange={onChangeHandler}
             required
           />
-        </label>
+        </label> */}
 
         <label>
-          When is the product published
-          <input
-            id={publishedToday}
-            type='date'
-            onChange={(e) => setPublishedToday(e.target.value)}
-            required
-          />
+          Is the product published today?
+          <label htmlFor='yes'>
+            <input
+              value='yes'
+              type='radio'
+              name='isPublishedToday'
+              onChange={(e) => setPublishedToday(e.target.value)}
+              id='yes'
+            />
+            yes
+          </label>
+          <label htmlFor='no'>
+            <input
+              value='no'
+              type='radio'
+              name='isPublishedToday'
+              onChange={(e) => setPublishedToday(e.target.value)}
+              id='no'
+            />
+            no
+          </label>
         </label>
 
         <input type='submit' />
       </form>
-
-      {calculatePrice({
-        userType = { user },
-        productType = { product },
-        price = { totalPrice },
-      })}
     </div>
   );
 };
