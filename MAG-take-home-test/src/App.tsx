03a06b51cb5500aa4966271eb09c6ba1e76b9814
Mattsson717 <83518778+Mattsson717@users.noreply.github.com>
2022-04-12 23:18:28 +0200

    started styling

--- MAG-take-home-test/src/App.tsx
+++ MAG-take-home-test/src/App.tsx
@@ -1,200 +1,142 @@
 import React, { useState, ChangeEvent } from 'react';
-import './App.css';
+import './App.scss';
+import Logo from './logo/Logo';
 
 const App = () => {
-  const [user, setUser] = useState<number | undefined>(0);
-  const [product, setProduct] = useState<number | undefined>(0);
+  const [user, setUser] = useState<string | undefined>('');
+  const [product, setProduct] = useState<string | undefined>('');
   const [publishedToday, setPublishedToday] = useState<string | undefined>(
-    'old'
+    'no'
   );
-  // const [price, setPrice] = useState<number | undefined>(0);
-  const [totalPrice, setTotalPrice] = useState<number | undefined>(0);
+  // const [price, setPrice] = useState<number | undefined>();
+  const [totalPrice, setTotalPrice] = useState<number | undefined>();
 
-  const productPrice: number = 200;
+  const productPrice: number = 100;
   const newProductPrice: number = 25;
   const oldProductPrice: number = 35;
   const companyRebate: number = 5;
   const publishedTodayRebate: number = 10;
+  // const priceInput: number = price;
 
-  const onChangeHandler = (e: ChangeEvent) => {
-    const newValue = (e.target as HTMLInputElement).value;
-  };
+  // const onChangeHandler = (e: ChangeEvent) => {
+  //   const newValue = (e.target as HTMLInputElement).valueAsNumber;
+  // };
 
   const calculatePrice = () => {
-    if (user === 1 && product === 0) {
+    if (user === 'normal' && product === 'new') {
       setTotalPrice(productPrice + newProductPrice - companyRebate);
-    } else if (user === 1 && product === 1) {
+    } else if (user === 'company' && product === 'old') {
       setTotalPrice(productPrice + oldProductPrice - companyRebate);
-    } else if (user === 0 && product === 0) {
+    } else if (user === 'normal' && product === 'new') {
       setTotalPrice(productPrice + newProductPrice);
     } else {
       setTotalPrice(productPrice + oldProductPrice);
     }
+
+    if (user === 'company' && product === 'new' && publishedToday === 'yes') {
+      setTotalPrice(
+        productPrice + newProductPrice - publishedTodayRebate - companyRebate
+      );
+    } else if (
+      user === 'normal' &&
+      product === 'old' &&
+      publishedToday === 'yes'
+    ) {
+      setTotalPrice(productPrice + newProductPrice - publishedTodayRebate);
+    }
   };
 
   return (
-    <div className='App'>
+    <section className='main'>
+      <Logo />
       <h1>Price Calculator</h1>
-      <form onSubmit={(e) => e.preventDefault()}>
-        <label>
-          Select user type:
-          <select defaultValue='default' onChange={onChangeHandler} required>
-            <option id='user' value='default' disabled>
-              Choose
-            </option>
+      <section className='calculator'>
+        <form onSubmit={(e) => e.preventDefault()}>
+          <div>
+            <label> Select usertype: </label>
+            <select
+              defaultValue='default'
+              onChange={(e) => setUser(e.target.value)}
+              required
+            >
+              <option id='user' value='default' disabled>
+                Choose
+              </option>
 
-            <option value={0}>Private user</option>
+              <option id='user' value='normal'>
+                Private user
+              </option>
 
-            <option value={1}>Company user</option>
-          </select>
-        </label>
+              <option id='user' value='company'>
+                Company user
+              </option>
+            </select>
+          </div>
+          <div>
+            <label>
+              Product type:
+              <select
+                defaultValue='default'
+                onChange={(e) => setProduct(e.target.value)}
+                required
+              >
+                <option id='productType' value='default' disabled>
+                  Choose
+                </option>
+                <option id='productType' value='new'>
+                  New
+                </option>
+                <option id='productType' value='old'>
+                  Old
+                </option>
+              </select>
+            </label>
+          </div>
 
-        <label>
-          Product type:
-          <select defaultValue='default' onChange={onChangeHandler} required>
-            <option id='productType' value='default' disabled>
-              Choose
-            </option>
-            <option id={'product'} value={0}>
-              New
-            </option>
-            <option id={'product'} value={1}>
-              Old
-            </option>
-          </select>
-        </label>
-
-        {/* <label>
+          {/* Can't get the onChangeHandler to recognize the setPrice */}
+          {/* <label>
           Price:
           <input
-            value={totalPrice}
+            value={price}
             type='number'
             onChange={onChangeHandler}
             required
           />
         </label> */}
+          <div>
+            <label>
+              Is the product published today?
+              <label htmlFor='yes'>
+                <input
+                  value='yes'
+                  type='radio'
+                  name='isPublishedToday'
+                  onChange={(e) => setPublishedToday(e.target.value)}
+                  id='yes'
+                />
+                yes
+              </label>
+              <label htmlFor='no'>
+                <input
+                  value='no'
+                  type='radio'
+                  name='isPublishedToday'
+                  onChange={(e) => setPublishedToday(e.target.value)}
+                  id='no'
+                />
+                no
+              </label>
+            </label>
+          </div>
+        </form>
 
-        <label>
-          Is the product published today?
-          <label htmlFor='yes'>
-            <input
-              value='yes'
-              type='radio'
-              name='isPublishedToday'
-              onChange={(e) => setPublishedToday(e.target.value)}
-              id='yes'
-            />
-            yes
-          </label>
-          <label htmlFor='no'>
-            <input
-              value='no'
-              type='radio'
-              name='isPublishedToday'
-              onChange={(e) => setPublishedToday(e.target.value)}
-              id='no'
-            />
-            no
-          </label>
-        </label>
-
-        <input type='submit' />
-      </form>
-    </div>
+        <div>Price: {totalPrice}</div>
+      </section>
+      <button className='submitBtn' type='submit' onClick={calculatePrice}>
+        Check price
+      </button>
+    </section>
   );
 };
 
 export default App;
-
-// const App = () => {
-//   const [user, setUser] = useState<string | undefined>('');
-//   const [product, setProduct] = useState<string | undefined>('');
-//   const [publishedToday, setPublishedToday] = useState<Date | undefined>(
-//     undefined
-//   );
-//   const [totalPrice, setTotalPrice] = useState<number | undefined>(0);
-
-//   return (
-//     <div className='App'>
-//       <h1>Price Calculator</h1>
-//       <form onSubmit={(e) => e.preventDefault()}>
-//         <label>
-//           Normal user:
-//           <input
-//             name='radio'
-//             type='radio'
-//             // id={userType}
-//             value={0}
-//             onChange={(e) => setUser(e.target.value)}
-//             // checked={userType}
-//           />
-//         </label>
-
-//         <label>
-//           Company user:
-//           <input
-//             name='radio'
-//             type='radio'
-//             // id={userType}
-//             value={1}
-//             onChange={(e) => setUser(e.target.value)}
-//             // checked={userTypeInput === userType}
-//           />
-//         </label>
-
-//         <label>
-//           Product type:
-//           <select
-//             defaultValue={'default'}
-//             onChange={(e) => setProduct(e.target.value)}
-//             required
-//           >
-//             <option
-//               // id={productType}
-//               value='default'
-//               disabled
-//             ></option>
-//             <option
-//               // id={productType}
-//               value={0}
-//             >
-//               New
-//             </option>
-//             <option
-//               // id={productType}
-//               value={1}
-//             >
-//               Old
-//             </option>
-//           </select>
-//         </label>
-
-//         <label>
-//           Price:
-//           <input
-//             // id={price}
-//             type='number'
-//             onChange={(e) => setProduct(e.target.value)}
-//             required
-//           />
-//         </label>
-
-//         <label>
-//           When is the product published
-//           <input
-//             // id={publishedDate}
-//             type='date'
-//             onChange={(e) => setProduct(e.target.value)}
-//             required
-//           />
-//         </label>
-
-//         <input type='submit' />
-//       </form>
-
-//       {/* {totalPrice} */}
-//     </div>
-//   );
-// };
-
-// export default App;
---
+++ MAG-take-home-test/src/logo/Logo.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Logo.scss';
+
+const Logo = () => {
+  return (
+    <div className='loader'>
+      <div className='loader-square'></div>
+      <div className='loader-square'></div>
+      <div className='loader-square'></div>
+      <div className='loader-square'></div>
+      <div className='loader-square'></div>
+      <div className='loader-square'></div>
+      <div className='loader-square'></div>
+    </div>
+  );
+};
+export default Logo;
